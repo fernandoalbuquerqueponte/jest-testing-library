797f1edaa55a45bb1120a257d4a09f161ffeace4
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2024-10-29 13:59:05 -0300

    feat: add name to list and remove name from the list

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,3 +1,44 @@
-test("sum", () => {
-  expect(1 + 1).toBe(2);
+import {
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import App from "./App";
+
+describe("App Component", () => {
+  it("should render list items", () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText("Fernando")).toBeInTheDocument();
+    expect(getByText("Caio")).toBeInTheDocument();
+    expect(getByText("Abreu")).toBeInTheDocument();
+  });
+
+  it("it should be able to add new item to the list", async () => {
+    const { getByText, getByPlaceholderText } = render(<App />);
+
+    const input = getByPlaceholderText("Novo Item");
+    const button = getByText("Adicionar");
+
+    await userEvent.type(input, "Adicionar");
+    await userEvent.click(button);
+
+    await waitFor(() => {
+      expect(getByText("Adicionar")).toBeInTheDocument();
+    });
+  });
+  it("it should be able to add remove item from the list", async () => {
+    const { getByText, getAllByText, queryByText } = render(<App />);
+
+    const button = getByText("Adicionar");
+
+    const removeButtons = getAllByText("Remover");
+
+    userEvent.click(removeButtons[0]);
+
+    await waitFor(() => {
+      expect(queryByText("Fernando")).not.toBeInTheDocument();
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 function App() {
+  const [list, setList] = useState(["Fernando", "Caio", "Abreu"]);
+  const [newItem, setNewItem] = useState("");
+
+  function addToList() {
+    setTimeout(() => {
+      setList((list) => [...list, newItem]);
+    }, 500);
+  }
+
+  function removeFromList(itemList: string) {
+    setTimeout(() => {
+      setList((list) => list.filter((item) => item !== itemList));
+    }, 500);
+  }
   return (
     <div>
-      <h1>Hello World!</h1>
+      <input
+        placeholder="Novo Item"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button onClick={addToList}>Adicionar</button>
+      <ul>
+        {list.map((item) => (
+          <li key={item}>
+            {item}
+            <button onClick={() => removeFromList(item)}>Remover</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
